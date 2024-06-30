53ee43fd59c15bc8ee3bdad092c474466cc9c33b
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-30 17:54:08 +0530

    Added Testimonial Page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Page3 from './pages/page3';
 import Page4 from './pages/page4';
 import Page5 from './pages/page5';
 import Page6 from './pages/page6';
+import Page7 from './pages/page7';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Page4 />
       <Page5 />
       <Page6 />
+      <Page7 />
     </>
   );
 }
