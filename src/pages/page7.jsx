8e3be815d5d836e8f13b80054280fa0fb31b711c
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-30 18:44:10 +0530

    Added the testimonial Page

--- src/pages/page7.jsx
+++ src/pages/page7.jsx
@@ -2,65 +2,15 @@ import Marquee from 'react-fast-marquee';
 export default function Page7() {
   return (
     <div className="page7">
-      <div className="page6heading">
+      <div className="page6heading pg7head">
         Student Voices: Experiences and Insights
       </div>
-      <div className="subheadpage6">
+      <div className="subheadpage6 pg7subhead">
         We're delighted by the smiles our students send us in response to our
         services.
       </div>
       <div className="testimonialslider">
-        <Marquee className="testmarquee" pauseOnHover={true} autoFill={true}>
-          <div className="testimonialcard">
-            <div className="toptestimonialcard">
-              <div className="studentimage">
-                <img
-                  src="studentimage1.png"
-                  alt="Students Images"
-                  className="studentimageimg"
-                />
-              </div>
-              <div className="studentinfo">
-                <div className="studentname">Abhishek Jain</div>
-                <div className="collegename">
-                  Vellore Institute of Technology , Vellore
-                </div>
-                <div className="intakedate">Intake - Sep 2022</div>
-              </div>
-            </div>
-            <div className="bottomtestimonialcard">
-              "Thanks to Gateway INC, I successfully secured admission to my
-              dream abroad college. Their dedicated support and expert guidance
-              made all the difference. I am grateful for their personalized
-              coaching and encouragement throughout the application process. I
-              couldn't have done it without them."
-            </div>
-          </div>
-          <div className="testimonialcard">
-            <div className="toptestimonialcard">
-              <div className="studentimage">
-                <img
-                  src="studentimage1.png"
-                  alt="Students Images"
-                  className="studentimageimg"
-                />
-              </div>
-              <div className="studentinfo">
-                <div className="studentname">Abhishek Jain</div>
-                <div className="collegename">
-                  Vellore Institute of Technology , Vellore
-                </div>
-                <div className="intakedate">Intake - Sep 2022</div>
-              </div>
-            </div>
-            <div className="bottomtestimonialcard">
-              "Thanks to Gateway INC, I successfully secured admission to my
-              dream abroad college. Their dedicated support and expert guidance
-              made all the difference. I am grateful for their personalized
-              coaching and encouragement throughout the application process. I
-              couldn't have done it without them."
-            </div>
-          </div>
+        <Marquee autoFill={true} speed={40} pauseOnHover={true}>
           <div className="testimonialcard">
             <div className="toptestimonialcard">
               <div className="studentimage">
@@ -213,61 +163,11 @@ export default function Page7() {
           </div>
         </Marquee>
         <Marquee
-          className="testmarquee"
-          pauseOnHover={true}
-          direction="right"
           autoFill={true}
+          direction="right"
+          speed={40}
+          pauseOnHover={true}
         >
-          <div className="testimonialcard">
-            <div className="toptestimonialcard">
-              <div className="studentimage">
-                <img
-                  src="studentimage1.png"
-                  alt="Students Images"
-                  className="studentimageimg"
-                />
-              </div>
-              <div className="studentinfo">
-                <div className="studentname">Abhishek Jain</div>
-                <div className="collegename">
-                  Vellore Institute of Technology , Vellore
-                </div>
-                <div className="intakedate">Intake - Sep 2022</div>
-              </div>
-            </div>
-            <div className="bottomtestimonialcard">
-              "Thanks to Gateway INC, I successfully secured admission to my
-              dream abroad college. Their dedicated support and expert guidance
-              made all the difference. I am grateful for their personalized
-              coaching and encouragement throughout the application process. I
-              couldn't have done it without them."
-            </div>
-          </div>
-          <div className="testimonialcard">
-            <div className="toptestimonialcard">
-              <div className="studentimage">
-                <img
-                  src="studentimage1.png"
-                  alt="Students Images"
-                  className="studentimageimg"
-                />
-              </div>
-              <div className="studentinfo">
-                <div className="studentname">Abhishek Jain</div>
-                <div className="collegename">
-                  Vellore Institute of Technology , Vellore
-                </div>
-                <div className="intakedate">Intake - Sep 2022</div>
-              </div>
-            </div>
-            <div className="bottomtestimonialcard">
-              "Thanks to Gateway INC, I successfully secured admission to my
-              dream abroad college. Their dedicated support and expert guidance
-              made all the difference. I am grateful for their personalized
-              coaching and encouragement throughout the application process. I
-              couldn't have done it without them."
-            </div>
-          </div>
           <div className="testimonialcard">
             <div className="toptestimonialcard">
               <div className="studentimage">
@@ -422,4 +322,31 @@ export default function Page7() {
       </div>
     </div>
   );
+}
+
+{
+  /* <div className="testimonialcard">
+  <div className="toptestimonialcard">
+    <div className="studentimage">
+      <img
+        src="studentimage1.png"
+        alt="Students Images"
+        className="studentimageimg"
+      />
+    </div>
+    <div className="studentinfo">
+      <div className="studentname">Abhishek Jain</div>
+      <div className="collegename">
+        Vellore Institute of Technology , Vellore
+      </div>
+      <div className="intakedate">Intake - Sep 2022</div>
+    </div>
+  </div>
+  <div className="bottomtestimonialcard">
+    "Thanks to Gateway INC, I successfully secured admission to my dream abroad
+    college. Their dedicated support and expert guidance made all the
+    difference. I am grateful for their personalized coaching and encouragement
+    throughout the application process. I couldn't have done it without them."
+  </div>
+</div>; */
 }
