330c56125efde8b1ad76e9b270347d41962a0b76
abhishekkjainn <jainabhishek1904@gmail.com>
2024-07-02 01:24:36 +0530

    Added footer

--- src/pages/page9.jsx
+++ src/pages/page9.jsx
@@ -2,11 +2,23 @@ export default function Page9() {
   return (
     <div className="page9">
       <div className="companyheadfooter">
-        <img src="/gatewaymainlogo2.png" alt="Logo" className="logofooter" />
-        <div className="companydescfooter">
-          A dedicated global education ally for both learners and institutions,
-          offering steadfast services, products, and solutions that support
-          learners throughout their educational pursuits worldwide.
+        <img src="gatewaymainlogo2.png" alt="" className="footerlogo" />
+        <div className="companysummaryfooter">
+          Consulting for Education with a Focus on Global Careers
+        </div>
+      </div>
+      <div className="addressandphn">
+        <div className="address">
+          <b>Gateway International </b>
+          <br />
+          115 Decora Square <br />
+          Opposite Sadhuvasvani School, Sadhuvasvani Road
+          <br />
+          Rajkot: 360005 <br />
+          Gujarat India
+        </div>
+        <div className="phnnofooter">
+          +91 7874709526 <br /> +91 7874709530
         </div>
       </div>
     </div>
