df0a3de8c81eb89834376e4d4d6abac3a1cda171
abhishekkjainn <jainabhishek1904@gmail.com>
2024-07-01 12:45:07 +0530

    Added the Consultation Form

--- src/pages/page8.jsx
+++ src/pages/page8.jsx
@@ -1,48 +1,39 @@
 export default function Page8() {
   return (
     <div className="page8">
-      <div className="formdiv">
-        <div className="formimage">
-          <img src="expert.png" alt="Expert" className="formimageimg" />
+      <div className="consultbanner">
+        <div className="consultimage">
+          <img src="expert.png" alt="Experts" className="expertimage" />
         </div>
-        <div className="contactformdiv">
-          <div className="form">
-            <div className="headingdiv">
-              <div className="headingtagform">
-                <div className="pulsediv">
-                  <div className="pulse"></div>
-                </div>
-                Book A Free Consulation
-              </div>
+        <div className="consultform">
+          <div className="consultformdiv">
+            <div className="consultheading">
+              Book A Free Consultation From Our Experts
             </div>
             <div className="inputdiv">
-              <div className="inputdivlabel">Full Name</div>
               <input
                 type="text"
-                name="fullname"
-                id="fullname"
-                className="inputdivinput"
+                name=""
+                id=""
+                className="inp"
+                placeholder="Enter Your Name"
               />
-            </div>
-            <div className="inputdiv">
-              <div className="inputdivlabel">Email</div>
-              <input
-                type="text"
-                name="fullname"
-                id="fullname"
-                className="inputdivinput"
-              />
-            </div>
-            <div className="inputdiv">
-              <div className="inputdivlabel">Phone Number</div>
               <input
                 type="number"
-                name="fullname"
-                id="fullname"
-                className="inputdivinput"
+                name=""
+                id=""
+                className="inp"
+                placeholder="Enter Your Phone"
               />
+              <input
+                type="email"
+                name=""
+                id=""
+                className="inp"
+                placeholder="Enter Your Email"
+              />
+              <div className="submitbutton">Book A Session</div>
             </div>
-            <div className="submitbutton">Submit</div>
           </div>
         </div>
       </div>
