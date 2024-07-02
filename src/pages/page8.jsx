1b7face8d4d4bb9aaa1331c731d6bdd75bf6dc98
abhishekkjainn <jainabhishek1904@gmail.com>
2024-07-02 13:38:10 +0530

    Added Consult form page

--- src/pages/page8.jsx
+++ src/pages/page8.jsx
@@ -3,36 +3,62 @@ export default function Page8() {
     <div className="page8">
       <div className="consultbanner">
         <div className="consultimage">
-          <img src="expert.png" alt="Experts" className="expertimage" />
+          <img src="expert.png" alt="" className="expertimage" />
         </div>
         <div className="consultform">
-          <div className="consultformdiv">
-            <div className="consultheading">
-              Book A Free Consultation From Our Experts
+          <div className="smallheading">
+            <div className="circleconsult"></div> Consult Experts
+          </div>
+          <div className="headingconsult">
+            Uncertain about what you need? Provide your details for a call-back.
+          </div>
+          <div className="formdiv">
+            <div className="namediv">
+              <div className="firstname">
+                <div className="iconinp">
+                  <img src="name.png" alt="Name" className="iconinpimage" />
+                </div>
+                <input
+                  className="inputconsult"
+                  type="text"
+                  placeholder="Enter First Name"
+                />
+              </div>
+              <div className="lastname">
+                <div className="iconinp">
+                  <img src="name.png" alt="Name" className="iconinpimage" />
+                </div>
+                <input
+                  className="inputconsult"
+                  type="text"
+                  placeholder="Enter Last Name"
+                />
+              </div>
             </div>
-            <div className="inputdiv">
-              <input
-                type="text"
-                name=""
-                id=""
-                className="inp"
-                placeholder="Enter Your Name"
-              />
-              <input
-                type="number"
-                name=""
-                id=""
-                className="inp"
-                placeholder="Enter Your Phone"
-              />
-              <input
-                type="email"
-                name=""
-                id=""
-                className="inp"
-                placeholder="Enter Your Email"
-              />
-              <div className="submitbutton">Book A Session</div>
+            <div className="namediv">
+              <div className="firstname">
+                <div className="iconinp">
+                  <img src="phone.png" alt="Name" className="iconinpimage" />
+                </div>
+                <input
+                  className="inputconsult"
+                  type="number"
+                  placeholder="Enter Phone No"
+                />
+              </div>
+              <div className="lastname">
+                <div className="iconinp">
+                  <img src="email.png" alt="Name" className="iconinpimage" />
+                </div>
+                <input
+                  className="inputconsult"
+                  type="email"
+                  placeholder="Enter Email ID"
+                />
+              </div>
+            </div>
+            <div className="submitbuttonconsult">
+              <div className="submitbuttonbutton">Book A Session</div>
             </div>
           </div>
         </div>
