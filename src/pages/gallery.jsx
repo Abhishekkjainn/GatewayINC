da2b805a99708b73c5dc2c0cc9a5d54e8ed7f8ea
abhishekkjainn <jainabhishek1904@gmail.com>
2024-07-06 15:50:01 +0530

    Added Gallery Page

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -6,187 +6,478 @@ export default function Gallery() {
       </div>
       <div className="galleryimagesdiv">
         <img
-          src="/gallery/Adit Mehta.png"
-          alt="Adit Mehta"
+          src="/gallery/Visa/Aenish Mukhida.png"
+          alt="Aenish Mukhida"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Akshat Parekh.png"
-          alt="Akshat Parekh"
+          src="/gallery/Visa/Afzal Padaniya.png"
+          alt="Afzal Padaniya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Akshay Dholariya.png"
-          alt="Akshay Dholariya"
-          className="galleryimageimg"
-        />
-        <img src="/gallery/Alvin.png" alt="Alvin" className="galleryimageimg" />
-        <img
-          src="/gallery/Anjali Pandya.png"
-          alt="Anjali Pandya"
+          src="/gallery/Visa/Ajay Godhaniya.png"
+          alt="Ajay Godhaniya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Anjali Patel.png"
-          alt="Anjali Patel"
+          src="/gallery/Visa/Ajijbhai Rathod (1).png"
+          alt="Ajjbhai Rathod (1)"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Anshita Zalavadia.png"
-          alt="Anshita Zalavadia"
+          src="/gallery/Visa/Alvin  Padaniya.png"
+          alt="Alvin Padaniya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Apurv Trivedi.png"
-          alt="Apurv Trivedi"
+          src="/gallery/Visa/Aryan Chandarana.png"
+          alt="Aryan Chandarana"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Arjun Modia.png"
-          alt="Arjun Modia"
-          className="galleryimageimg"
-        />
-        <img src="/gallery/Aryan.png" alt="Aryan" className="galleryimageimg" />
-        <img src="/gallery/Asim.png" alt="Asim" className="galleryimageimg" />
-        <img
-          src="/gallery/Avi Sanepara.png"
-          alt="Avi Sanepara"
+          src="/gallery/Visa/Asifkha.png"
+          alt="Asifkha"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Bhavya Mungra.png"
-          alt="Bhavya Mungra"
+          src="/gallery/Visa/bhagyashree (1).png"
+          alt="bhagyashree (1)"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Brijesh Somaiya.png"
-          alt="Brijesh Somaiya"
+          src="/gallery/Visa/Bhatt Family.png"
+          alt="Bhatt Family"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Chintan Patel.png"
-          alt="Chintan Patel"
+          src="/gallery/Visa/bonny.png"
+          alt="bonny"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Darshit Vachhani.png"
-          alt="Darshit Vachhani"
+          src="/gallery/Visa/Brinda.png"
+          alt="Brinda"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Deep Amruutiya.png"
-          alt="Deep Amrutiya"
+          src="/gallery/Visa/Dharvi Kansagara.png"
+          alt="Dharvi Kansagara"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Deep Bhoraniya.png"
-          alt="Deep Bhoraniya"
+          src="/gallery/Visa/Dhruv Kamdar.png"
+          alt="Dhruv Kamdar"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Deep Mashru.png"
-          alt="Deep Mashru"
-          className="galleryimageimg"
-        />
-        <img
-          src="/gallery/Deepak Pal.png"
-          alt="Deepak Pal"
-          className="galleryimageimg"
-        />
-        <img
-          src="/gallery/Devang Gandhi.png"
-          alt="Devang Gandhi"
-          className="galleryimageimg"
-        />
-        <img
-          src="/gallery/Devansh Acharya.png"
-          alt="Devansh Acharya"
-          className="galleryimageimg"
-        />
-        <img
-          src="/gallery/Dhara Suvagiya.png"
-          alt="Dhara Suvagiya"
-          className="galleryimageimg"
-        />
-        <img
-          src="/gallery/Dharti Vachhani.png"
-          alt="Dharti Vachhani"
-          className="galleryimageimg"
-        />
-        <img src="/gallery/Dhruv.png" alt="Dhruv" className="galleryimageimg" />
-        <img src="/gallery/Dipam.png" alt="Dipam" className="galleryimageimg" />
-        <img
-          src="/gallery/Gaurav Soni.png"
-          alt="Gaurav Soni"
-          className="galleryimageimg"
-        />
-        <img
-          src="/gallery/Greesha Soni.png"
+          src="/gallery/Visa/Greesha Soni.png"
           alt="Greesha Soni"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Hari Pansuriya.png"
-          alt="Hari Pansuriya"
+          src="/gallery/Visa/Harshith Poojary.png"
+          alt="Harshith Poojary"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Harshit Mungra.png"
-          alt="Harshit Mungra"
+          src="/gallery/Visa/Hema Pansuriya.png"
+          alt="Hema Pansuriya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Henil Khunt.png"
-          alt="Henil Khunt"
+          src="/gallery/Visa/Himanshu Dafda (1).png"
+          alt="Himanshu Dafda (1)"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Heti Bhatt.png"
-          alt="Heti Bhatt"
+          src="/gallery/Visa/Ishita Kachhadiya.png"
+          alt="Ishita Kachhadiya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Hima Sutaria.png"
-          alt="Hima Sutaria"
+          src="/gallery/Visa/Jaiteexa Rathod.png"
+          alt="Jaiteexa Rathod"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Himanshu Dafda.png"
-          alt="Himanshu Dafda"
+          src="/gallery/Visa/Janki Rathod.png"
+          alt="Janki Rathod"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Jhanvi Khaniya.png"
+          src="/gallery/Visa/Jaydeep Unagar.png"
+          alt="Jaydeep Unagar"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Jevin.png"
+          alt="Jevin"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Jhanvi Khaniya.png"
           alt="Jhanvi Khaniya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Krishna Jani.png"
-          alt="Krishna Jani"
+          src="/gallery/Visa/Jitendra Pujara.png"
+          alt="Jitendra Pujara"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Maharshi Vyas.png"
+          src="/gallery/Visa/Joshi Family (1).png"
+          alt="Joshi Family (1)"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/kajol Trivedi.png"
+          alt="kajol Trivedi"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Khush Bhayani.png"
+          alt="Khush Bhayani"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Kunj Dhami.png"
+          alt="Kunj Dhami"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Maharshi Vyas.png"
           alt="Maharshi Vyas"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Milan Ramoliya.png"
+          src="/gallery/Visa/Maharshi.png"
+          alt="Maharshi"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Manas Thaker.png"
+          alt="Manas Thaker"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Mehreen Rupani.png"
+          alt="Mehreen Rupani"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Mitul Vaghela.png"
+          alt="Mitul Vaghela"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Nimit Mehta.png"
+          alt="Nimit Mehta"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Nisarg Chudasama.png"
+          alt="Nisarg Chudasama"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Parmar Family.png"
+          alt="Parmar Family"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Pratp.png"
+          alt="Pratp"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Prince Kalariya.png"
+          alt="Prince Kalariya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Raj Charaniya.png"
+          alt="Raj Charaniya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Raj Teraiya.png"
+          alt="Raj Teraiya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Rajat Dahiya.png"
+          alt="Rajat Dahiya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Ramde.png"
+          alt="Ramde"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Rikita.png"
+          alt="Rikita"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/satapara family.png"
+          alt="satapara family"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Sejal Patel.png"
+          alt="Sejal Patel"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Shraddha.png"
+          alt="Shraddha"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Siddharth Shukla.png"
+          alt="Siddharth Shukla"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Urvish Mistry.png"
+          alt="Urvish Mistry"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Vaibhavi Pujara.png"
+          alt="Vaibhavi Pujara"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Vanita.png"
+          alt="Vanita"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Viralpuri Gosai.png"
+          alt="Viralpuri Gosai"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Vishal Usadad.png"
+          alt="Vishal Usadad"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Yash Ajudiya.png"
+          alt="Yash Ajudiya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/Visa/Zalavadia Family.png"
+          alt="Zalavadia Family"
+          className="galleryimageimg"
+        />
+
+        <img
+          src="/gallery/New IELTS/Adit Mehta.png"
+          alt="Adit Mehta"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Akshat Parekh.png"
+          alt="Akshat Parekh"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Akshay Dholariya.png"
+          alt="Akshay Dholariya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Alvin.png"
+          alt="Alvin"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Anjali Pandya.png"
+          alt="Anjali Pandya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Anjali Patel.png"
+          alt="Anjali Patel"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Anshita Zalavadia.png"
+          alt="Anshita Zalavadia"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Apurv Trivedi.png"
+          alt="Apurv Trivedi"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Arjun Modia.png"
+          alt="Arjun Modia"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Aryan.png"
+          alt="Aryan"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Asim.png"
+          alt="Asim"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Avi Sanepara.png"
+          alt="Avi Sanepara"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Bhavya Mungra.png"
+          alt="Bhavya Mungra"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Brijesh Somaiya.png"
+          alt="Brijesh Somaiya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Chintan Patel.png"
+          alt="Chintan Patel"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Darshit Vachhani.png"
+          alt="Darshit Vachhani"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Deep Amruutiya.png"
+          alt="Deep Amrutiya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Deep Bhoraniya.png"
+          alt="Deep Bhoraniya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Deep Mashru.png"
+          alt="Deep Mashru"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Deepak Pal.png"
+          alt="Deepak Pal"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Devang Gandhi.png"
+          alt="Devang Gandhi"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Devansh Acharya.png"
+          alt="Devansh Acharya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Dhara Suvagiya.png"
+          alt="Dhara Suvagiya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Dharti Vachhani.png"
+          alt="Dharti Vachhani"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Dhruv.png"
+          alt="Dhruv"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Dipam.png"
+          alt="Dipam"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Gaurav Soni.png"
+          alt="Gaurav Soni"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Greesha Soni.png"
+          alt="Greesha Soni"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Hari Pansuriya.png"
+          alt="Hari Pansuriya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Harshit Mungra.png"
+          alt="Harshit Mungra"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Henil Khunt.png"
+          alt="Henil Khunt"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Heti Bhatt.png"
+          alt="Heti Bhatt"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Hima Sutaria.png"
+          alt="Hima Sutaria"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Himanshu Dafda.png"
+          alt="Himanshu Dafda"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Jhanvi Khaniya.png"
+          alt="Jhanvi Khaniya"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Krishna Jani.png"
+          alt="Krishna Jani"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Maharshi Vyas.png"
+          alt="Maharshi Vyas"
+          className="galleryimageimg"
+        />
+        <img
+          src="/gallery/New IELTS/Milan Ramoliya.png"
           alt="Milan Ramoliya"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Renita Nagani.png"
+          src="/gallery/New IELTS/Renita Nagani.png"
           alt="Renita Nagani"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Shraddha Khunti.png"
+          src="/gallery/New IELTS/Shraddha Khunti.png"
           alt="Shraddha Khunti"
           className="galleryimageimg"
         />
         <img
-          src="/gallery/Vatsal Makadia.png"
+          src="/gallery/New IELTS/Vatsal Makadia.png"
           alt="Vatsal Makadia"
           className="galleryimageimg"
         />
